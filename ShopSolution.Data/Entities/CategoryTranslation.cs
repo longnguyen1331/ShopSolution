9a9442c99cbe4fd6c5a1ef8a9fa0bfca6fd64f4b
nguyen ho phi long <longnguyen13th1d.pou@gmail.com>
2020-02-02 15:50:22 +0700

    Migration Database

--- ShopSolution.Data/Entities/CategoryTranslation.cs
+++ ShopSolution.Data/Entities/CategoryTranslation.cs
@@ -9,7 +9,7 @@ namespace ShopSolution.Data.Entities
         public int CategoryId { set; get; }
         public string SeoDescription { set; get; }
         public string SeoTitle { set; get; }
-        public string LanguageId { set; get; }
+        public int LanguageId { set; get; }
         public string SeoAlias { set; get; }
 
         public Category Category { get; set; }
